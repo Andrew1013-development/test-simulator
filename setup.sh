910ed0f56a0d9b06cac156425d51e5182444f53d
Tran Viet Huy <73055116+Andrew1013-development@users.noreply.github.com>
2023-06-12 21:37:07 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+echo "test-simulator download script for Linux / macOS [PROTOTYPE]"
+echo "test script only, not for production use yet"
+echo $PWD
+echo "Downloading on current directory....."
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/file_copier.py -o file_copier.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/file_seeker.py -o file_seeker.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/file_generator.py -o file_generator.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/file_remover.py -o file_remover.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/file_sorter.py -o file_sorter.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/file_sorter_2.py -o file_sorter_2.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/file_remover_2.py -o file_remover_2.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/sus_runner.py -o sus_runner.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/plotter.py -o plotter.py
+curl https://raw.githubusercontent.com/Andrew1013-development/python-tester/main/runner.py -o runner.py
+echo "Done!"
