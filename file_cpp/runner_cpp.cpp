a635383aef3dfc557d255b2bff9b87d4f6829e75
Andrew1013-development <viethuytran147@gmail.com>
2023-06-21 16:25:37 +0700

    more fixes

--- file_cpp/runner_cpp.cpp
+++ file_cpp/runner_cpp.cpp
@@ -8,7 +8,7 @@
 #include "modules.hpp"
 using namespace std;
 
-const string version = "1.0.0-WIP4";
+const string version = "1.0.0-WIP6";
 
 int main(int argc, char** argv) {
     signal(SIGINT, exit); //bind SIGINT (Ctrl-C) to exit
@@ -22,40 +22,43 @@ int main(int argc, char** argv) {
 
     // input (type-cast the hell outta this because C++ thinks an array of chars is different from a string)
     // also damn C++ finally being more accessible once you get to know the STL huh
-    test_dir = argv[1];
-    if ((string)argv[2] == "-debug") {
-        dbg_flag = true;
-    } else {
-        if ((string)argv[2] == "-nodebug") {
-            dbg_flag = false;
+    if (argc == 4) {
+        test_dir = argv[1];
+        if ((string)argv[2] == "-debug") {
+            dbg_flag = true;
         } else {
-            cout << "invalid choice for debug flag." << endl;
+            if ((string)argv[2] == "-nodebug") {
+                dbg_flag = false;
+            } else {
+                cout << "invalid choice for debug flag." << endl;
+            }
         }
+        if ((string)argv[3] == "-fulldebug") {
+            dbgfull_flag = true;
+        } else {
+            if ((string)argv[3] == "-nofulldebug") {
+                dbgfull_flag = false;
+            } else {
+                cout << "invaild choice for full debug flag" << endl;
+            }
+        } 
+        n_iters = stoul(argv[4]); //convert string to unsigned long
+
+        // main code
+        auto start = chrono::high_resolution_clock::now();
+        tuple<double, unsigned long> generator_result = file_cpp::generator_cpp(test_dir,dbg_flag,dbgfull_flag,n_iters);
+        double sorter_time = file_cpp::sorter_cpp(test_dir,dbg_flag,dbgfull_flag);
+        double remover_time = file_cpp::remover_cpp(test_dir,dbg_flag,dbgfull_flag);
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+        double duration_num = duration.count() / pow(10,9);
+
+        // set number output mode to have actual decimal points (another fuck you C++)
+        cout << fixed << setprecision(3);
+        cout << get<0>(generator_result) << " " << sorter_time << " " << remover_time << " " << get<1>(generator_result) << endl;
+        cout << duration_num << endl;
+    } else {
+        cout << "no runner_cpp for you." << endl;
     }
-    if ((string)argv[3] == "-fulldebug") {
-        dbgfull_flag = true;
-    } else {
-        if ((string)argv[3] == "-nofulldebug") {
-            dbgfull_flag = false;
-        } else {
-            cout << "invaild choice for full debug flag" << endl;
-        }
-    } 
-    n_iters = stoul(argv[4]); //convert string to unsigned long
-
-    // main code
-    auto start = chrono::high_resolution_clock::now();
-    tuple<double, unsigned long> generator_result = file_cpp::generator_cpp(test_dir,dbg_flag,dbgfull_flag,n_iters);
-    double sorter_time = file_cpp::sorter_cpp(test_dir,dbg_flag,dbgfull_flag);
-    double remover_time = file_cpp::remover_cpp(test_dir,dbg_flag,dbgfull_flag);
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
-    double duration_num = duration.count() / pow(10,9);
-
-    // set number output mode to have actual decimal points (another fuck you C++)
-    cout << fixed << setprecision(3);
-    cout << get<0>(generator_result) << " " << sorter_time << " " << remover_time << " " << get<1>(generator_result) << endl;
-    cout << duration_num << endl;
-    
     return 0;
 }
