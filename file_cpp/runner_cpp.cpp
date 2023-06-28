ee0aae992dca86d1fa8eee307ba3b33c5f582f92
Andrew1013-development <viethuytran147@gmail.com>
2023-06-28 16:11:20 +0700

    quality-of-code improvement to C++ reimplementation

--- file_cpp/runner_cpp.cpp
+++ file_cpp/runner_cpp.cpp
@@ -7,8 +7,47 @@
 #include <string>
 #include <csignal>
 #include "modules.hpp"
-#define VERSION "1.0.4"
+#define VERSION "1.1.0"
 using namespace std;
+
+void runner(string directory, bool debug_short, bool debug_full, unsigned long dates, bool file_output) {
+    vector<double> time_results;
+    
+    auto start = chrono::high_resolution_clock::now();
+    tuple<double, unsigned long, vector<string>> generator_result = file_cpp::generator_cpp(directory,debug_short,debug_full,dates);
+    double sorter_time = file_cpp::sorter_cpp(directory,get<2>(generator_result),debug_short,debug_full);
+    double remover_time = file_cpp::remover_cpp(directory,debug_short,debug_full);
+    auto end = chrono::high_resolution_clock::now();
+    
+    auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    double duration_num = duration.count() / pow(10,9);
+    double delta_time = duration_num - get<0>(generator_result) - sorter_time - remover_time; 
+    
+    // result collection
+    time_results.push_back(dates);
+    time_results.push_back(duration_num);
+    time_results.push_back(get<0>(generator_result));
+    time_results.push_back(sorter_time);
+    time_results.push_back(remover_time);
+    time_results.push_back(delta_time);
+    time_results.push_back(get<1>(generator_result));
+    
+    // set number output mode to have actual decimal points (another fuck you C++)
+    cout << fixed << setprecision(3);
+    cout << "----------------------------EXECUTION INFORMATION (C++ REIMPLEMENTATION)---------------------------" << endl;
+    cout << "Total time to execute all 3 functions (runner time): " << duration_num  << " seconds" << endl;
+    cout << "Individual time of each segment (individual function time):" << endl;
+    cout << "\tGenerator : " << get<0>(generator_result) << " seconds (" << get<0>(generator_result) / duration_num * 100 << "% of runtime)" << endl; 
+    cout << "\tSorter : " << sorter_time << " seconds (" << sorter_time / duration_num * 100 << "% of runtime)" << endl;
+    cout << "\tRemover : " << remover_time << " seconds (" << remover_time / duration_num * 100 << "% of runtime)" << endl;
+    cout << "\tTime dilation (delta) : " << delta_time << " seconds (" << delta_time / duration_num * 100 << "% of runtime)" << endl; 
+    cout << "Files sorted: " << get<1>(generator_result) << endl;
+    cout << endl;
+    
+    if (file_output) {
+        file_cpp::file_output(&time_results, "runtime_iterations_information_cpp.txt");
+    }
+}
 
 int main(int argc, char** argv) {
     signal(SIGINT, exit); //bind SIGINT (Ctrl-C) to exit
@@ -17,8 +56,7 @@ int main(int argc, char** argv) {
     bool dbgfull_flag = false;
     bool file_output = true;
     unsigned long n_iters = 1;
-    vector<double> time_results;
-
+    
     // input (type-cast the hell outta this because C++ thinks an array of chars is different from a string)
     // also damn C++ finally being more accessible once you get to know the STL huh
     if (argc == 6) {
@@ -52,39 +90,7 @@ int main(int argc, char** argv) {
             }
         }
         // main code
-        auto start = chrono::high_resolution_clock::now();
-        tuple<double, unsigned long, vector<string>> generator_result = file_cpp::generator_cpp(test_dir,dbg_flag,dbgfull_flag,n_iters);
-        double sorter_time = file_cpp::sorter_cpp(test_dir,get<2>(generator_result),dbg_flag,dbgfull_flag);
-        double remover_time = file_cpp::remover_cpp(test_dir,dbg_flag,dbgfull_flag);
-        auto end = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
-        double duration_num = duration.count() / pow(10,9);
-        double delta_time = duration_num - get<0>(generator_result) - sorter_time - remover_time; 
-        
-        // result collection
-        time_results.push_back(n_iters);
-        time_results.push_back(duration_num);
-        time_results.push_back(get<0>(generator_result));
-        time_results.push_back(sorter_time);
-        time_results.push_back(remover_time);
-        time_results.push_back(delta_time);
-        time_results.push_back(get<1>(generator_result));
-        
-        // set number output mode to have actual decimal points (another fuck you C++)
-        cout << fixed << setprecision(3);
-        cout << "----------------------------EXECUTION INFORMATION (C++ REIMPLEMENTATION)---------------------------" << endl;
-        cout << "Total time to execute all 3 functions (runner time): " << duration_num  << " seconds" << endl;
-        cout << "Individual time of each segment (individual function time):" << endl;
-        cout << "\tGenerator : " << get<0>(generator_result) << " seconds (" << get<0>(generator_result) / duration_num * 100 << "% of runtime)" << endl; 
-        cout << "\tSorter : " << sorter_time << " seconds (" << sorter_time / duration_num * 100 << "% of runtime)" << endl;
-        cout << "\tRemover : " << remover_time << " seconds (" << remover_time / duration_num * 100 << "% of runtime)" << endl;
-        cout << "\tTime dilation (delta) : " << delta_time << " seconds (" << delta_time / duration_num * 100 << "% of runtime)" << endl; 
-        cout << "Files sorted: " << get<1>(generator_result) << endl;
-
-        if (file_output) {
-            file_cpp::file_output(&time_results, "runtime_iterations_information_cpp.txt");
-        }
-        cout << endl;
+        runner(test_dir,dbg_flag,dbgfull_flag,n_iters,file_output);
     } else {
         cout << "no runner_cpp for you." << endl;
     }
--- file_cpp/sus_runner_cpp.cpp
+++ file_cpp/sus_runner_cpp.cpp
@@ -7,15 +7,42 @@
 #include <string>
 #include <csignal>
 #include "modules.hpp"
-#define VERSION "1.1.3"
+#define VERSION "1.2.0"
 using namespace std;
+
+void sus_runner(string directory, bool debug_short, bool debug_full) {
+    // main code
+    //tuple<double, unsigned long, vector<string>> generator_result = file_cpp::generator_cpp(directory,debug_short,debug_full,n_iters);
+    //double copier_time = file_cpp::copier_cpp(directory,directory2,debug_short,debug_full);
+    auto start = chrono::high_resolution_clock::now();
+    tuple<double, vector<string>, unsigned long> seeker_result = file_cpp::seeker_cpp(directory,debug_short,debug_full);
+    double sorter_time = file_cpp::sorter_cpp(directory,get<1>(seeker_result),debug_short,debug_full);
+    double remover_time = file_cpp::remover_cpp(directory,debug_short,debug_full);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    double duration_num = duration.count() / pow(10,9);
+    //file_cpp::remover_cpp(test_dir2,dbg_flag,dbgfull_flag);
+    
+    // set number output mode to have actual decimal points (another fuck you C++)
+    cout << fixed << setprecision(3);
+    cout << "----------------------------EXECUTION INFORMATION (C++ REIMPLEMENTATION)---------------------------" << endl;
+    cout << "Total time to execute all 3 functions (runner time): " << duration_num  << " seconds" << endl;
+    cout << "Individual time of each segment (individual function time):" << endl;
+    //cout << "\tGenerator : " << get<0>(generator_result) << " seconds (" << get<0>(generator_result) / duration_num * 100 << "% of runtime)" << endl; 
+    //cout << "\t Copier : " << copier_time << " seconds (" << copier_time / duration_num * 100 << "% of runtime)" << endl;
+    cout << "\tSeeker : " << get<0>(seeker_result) << " seconds (" << get<0>(seeker_result) / duration_num * 100 << "% of runtime)" << endl;
+    cout << "\tSorter : " << sorter_time << " seconds (" << sorter_time / duration_num * 100 << "% of runtime)" << endl;
+    cout << "\tRemover : " << remover_time << " seconds (" << remover_time / duration_num * 100 << "% of runtime)" << endl;
+    cout << "Files sorted: " << get<2>(seeker_result) << endl;
+    cout << endl;
+}
 
 int main(int argc, char** argv) {
     signal(SIGINT, exit); //bind SIGINT (Ctrl-C) to exit
     string test_dir;
     bool dbg_flag = false;
     bool dbgfull_flag = false;
-    unsigned long n_iters = 1;
+    //unsigned long n_iters = 1;
 
     // input (type-cast the hell outta this because C++ thinks an array of chars is different from a string)
     // also damn C++ finally being more accessible once you get to know the STL huh
@@ -39,32 +66,8 @@ int main(int argc, char** argv) {
                 cout << "invaild choice for full debug flag" << endl;
             }
         } 
-        n_iters = stoul(argv[4]); //convert string to unsigned long
-
-        // main code
-        //tuple<double, unsigned long, vector<string>> generator_result = file_cpp::generator_cpp(test_dir,dbg_flag,dbgfull_flag,n_iters);
-        //double copier_time = file_cpp::copier_cpp(test_dir,test_dir2,dbg_flag,dbgfull_flag);
-        auto start = chrono::high_resolution_clock::now();
-        tuple<double, vector<string>, unsigned long> seeker_result = file_cpp::seeker_cpp(test_dir,dbg_flag,dbgfull_flag);
-        double sorter_time = file_cpp::sorter_cpp(test_dir,get<1>(seeker_result),dbg_flag,dbgfull_flag);
-        double remover_time = file_cpp::remover_cpp(test_dir,dbg_flag,dbgfull_flag);
-        auto end = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
-        double duration_num = duration.count() / pow(10,9);
-        //file_cpp::remover_cpp(test_dir2,dbg_flag,dbgfull_flag);
-        
-        // set number output mode to have actual decimal points (another fuck you C++)
-        cout << fixed << setprecision(3);
-        cout << "----------------------------EXECUTION INFORMATION (C++ REIMPLEMENTATION)---------------------------" << endl;
-        cout << "Total time to execute all 3 functions (runner time): " << duration_num  << " seconds" << endl;
-        cout << "Individual time of each segment (individual function time):" << endl;
-        //cout << "\tGenerator : " << get<0>(generator_result) << " seconds (" << get<0>(generator_result) / duration_num * 100 << "% of runtime)" << endl; 
-        //cout << "\t Copier : " << copier_time << " seconds (" << copier_time / duration_num * 100 << "% of runtime)" << endl;
-        cout << "\tSeeker : " << get<0>(seeker_result) << " seconds (" << get<0>(seeker_result) / duration_num * 100 << "% of runtime)" << endl;
-        cout << "\tSorter : " << sorter_time << " seconds (" << sorter_time / duration_num * 100 << "% of runtime)" << endl;
-        cout << "\tRemover : " << remover_time << " seconds (" << remover_time / duration_num * 100 << "% of runtime)" << endl;
-        cout << "Files sorted: " << get<2>(seeker_result) << endl;
-        cout << endl;
+        //n_iters = stoul(argv[4]); //convert string to unsigned long
+        sus_runner(test_dir,dbg_flag,dbgfull_flag);        
     } else {
         cout << "no sus_runner_cpp for you." << endl;
     }
